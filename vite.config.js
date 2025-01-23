9628e4126595cc978ce44a508355bc44d2837ccb
Bhautik Mangukiya <bhautikmangukiya3386@gmail.com>
2025-01-23 12:21:57 +0530

    My game

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/React_Tic_Tac_Toe"
-})
-
+  base: '/React_Tic_Tac_Toe/', // Replace with your repository name
+});
